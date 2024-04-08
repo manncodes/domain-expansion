39b75aa3387ca087c7dc18a7f70111c2ca792676
manncodes <manncodes@gmail.com>
2024-04-08 01:59:44 -0700

    references

---
+++ eval/mapping.py
@@ -0,0 +1,20 @@
+label2id = {
+    "Strongly Disagree": 0,
+    "Disagree": 1,
+    "Slightly Disagree": 2,
+    "Neutral": 3,
+    "Slightly Agree": 4,
+    "Agree": 5,
+    "Strongly Agree": 6
+}
+
+id2label = {
+    0: "Strongly Disagree",
+    1: "Disagree",
+    2: "Slightly Disagree",
+    3: "Neutral",
+    4: "Slightly Agree",
+    5: "Agree",
+    6: "Strongly Agree"
+    
+}
